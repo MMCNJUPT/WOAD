11cef2bfcde5039f883aa644de5963ee75c47ece
MMCNJUPT <2020010305liu@gmail.com>
2024-05-31 15:17:37 +0800

    Delete Hardware deployment directory

--- Hardware deployment/sim/RL/GEMMR1_tb.f
+++
@@ -1,7 +0,0 @@
-/home/nitcloud/project/FPGA/Design/HDL/Tactical-helmet/user/sim/RL/GEMMR1_tb.v
-/home/nitcloud/project/FPGA/Design/HDL/Tactical-helmet/user/src/process/RL/GEMMR1.v
-/home/nitcloud/project/FPGA/Design/HDL/Tactical-helmet/user/src/process/RL/ROM/gemm1L.v
-/home/nitcloud/project/FPGA/Design/HDL/Tactical-helmet/user/src/process/RL/utils.v
-/home/nitcloud/project/FPGA/Design/HDL/Tactical-helmet/user/src/utils/math/utils.v
-/home/nitcloud/project/FPGA/Design/HDL/Tactical-helmet/user/src/utils/math/timing/tmult.v
-/home/nitcloud/project/FPGA/Design/HDL/Tactical-helmet/user/src/utils/math/timing/tadd.v
